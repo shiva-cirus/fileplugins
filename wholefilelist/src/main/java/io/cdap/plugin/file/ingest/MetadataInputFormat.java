ef661e5a84af44a5e83508d5d2954018f3b88ae1
Pawan Goyal <pawan.goyal@amantechnologiesinc.com>
2019-11-02 05:12:37 +0530

    remove directory

--- wholefilelist/src/main/java/io/cdap/plugin/file/ingest/MetadataInputFormat.java
+++ wholefilelist/src/main/java/io/cdap/plugin/file/ingest/MetadataInputFormat.java
@@ -1,12 +1,12 @@
 /*
  * Copyright © 2017 Cask Data, Inc.
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *  
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -17,16 +17,8 @@
 package io.cdap.plugin.file.ingest;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.LocatedFileStatus;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.RemoteIterator;
-import org.apache.hadoop.mapreduce.InputFormat;
-import org.apache.hadoop.mapreduce.InputSplit;
-import org.apache.hadoop.mapreduce.JobContext;
-import org.apache.hadoop.mapreduce.RecordReader;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.mapreduce.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,11 +29,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.stream.Collectors;
 
 /**
- * Abstract class that implements the inputFormat for the FileCopySource plugin to
- * read file metadata. The getsplit method creates splits according to user-set configuration
- * and tries to assign files to each split such that every split copies roughly the same number of bytes.
+ * Abstract class that implements the inputFormat for the FileCopySource plugin to read file
+ * metadata. The getsplit method creates splits according to user-set configuration and tries to
+ * assign files to each split such that every split copies roughly the same number of bytes.
  */
 public class MetadataInputFormat extends InputFormat {
 
@@ -73,17 +66,18 @@ public class MetadataInputFormat extends InputFormat {
   }
 
   @Override
-  public RecordReader createRecordReader(InputSplit inputSplit, TaskAttemptContext taskAttemptContext)
-    throws IOException, InterruptedException {
+  public RecordReader createRecordReader(
+      InputSplit inputSplit, TaskAttemptContext taskAttemptContext)
+      throws IOException, InterruptedException {
     MetadataRecordReader recordReader = new MetadataRecordReader();
     recordReader.initialize(inputSplit, taskAttemptContext);
     return recordReader;
   }
 
   /**
-   * This method scans the files under the directories specified by the user. Splits are created given the total
-   * number of files and the number of files each split can contain. The files are then assigned to splits such that
-   * each split gets roughly the same number of bytes.
+   * This method scans the files under the directories specified by the user. Splits are created
+   * given the total number of files and the number of files each split can contain. The files are
+   * then assigned to splits such that each split gets roughly the same number of bytes.
    *
    * @param jobContext Contains the configurations specified by the user.
    * @return The list of splits.
@@ -91,7 +85,8 @@ public class MetadataInputFormat extends InputFormat {
    * @throws InterruptedException
    */
   @Override
-  public List<InputSplit> getSplits(JobContext jobContext) throws IOException, InterruptedException {
+  public List<InputSplit> getSplits(JobContext jobContext)
+      throws IOException, InterruptedException {
     // connect to the source filesystem
     Configuration conf = jobContext.getConfiguration();
     String[] sourcePaths = conf.get(SOURCE_PATHS).split(",");
@@ -111,12 +106,16 @@ public class MetadataInputFormat extends InputFormat {
     // scan the directories specified by the user
     List<FileMetadata> fileMetaDataList = new ArrayList<>();
     for (String prefix : sourcePaths) {
-      recursivelyAddFileStatus(fileMetaDataList, prefix, new Path(prefix), recursive, fileSystem, conf);
+      recursivelyAddFileStatus(
+          fileMetaDataList, prefix, new Path(prefix), recursive, fileSystem, conf);
     }
 
-    // sort fileMetadataList in descending order such that total number of bytes can be more evenly distributed
+    // sort fileMetadataList in descending order such that total number of bytes can be more evenly
+    // distributed
     Collections.sort(fileMetaDataList);
     Collections.reverse(fileMetaDataList);
+
+    fileMetaDataList= fileMetaDataList.stream().filter(fileMetadata -> !fileMetadata.isDir()).collect(Collectors.toList());
 
     // compute number of splits and instantiate the splits
     // We use a priority queue to keep track of the smallest split (fewest bytes assigned to it)
@@ -150,27 +149,34 @@ public class MetadataInputFormat extends InputFormat {
 
   /**
    * Because the existing Filesystem.listFiles(Path, Boolean) doesn't list empty directories, we
-   * added our own method to recursively traverse the file directories. If the path doesn't exist
-   * in the source filesystem, it logs a warning and skips the path.
+   * added our own method to recursively traverse the file directories. If the path doesn't exist in
+   * the source filesystem, it logs a warning and skips the path.
    *
    * @param fileMetadataList The list that contains all the files under fileStatus.getPath
    * @param prefix The user-set path that was used to get this group of files
    * @param path The path of the file that will be inserted into fileMetadataList.
    * @param recursive Whether or not to recursively scan the directories.
    * @param filesystem The filesystem that contains the files.
-   * @param conf The configuration that contains credential information needed to connect to the filesystem.
+   * @param conf The configuration that contains credential information needed to connect to the
+   *     filesystem.
    * @throws IOException
    */
-  private void recursivelyAddFileStatus(List<FileMetadata> fileMetadataList,
-                                         String prefix, Path path, Boolean recursive,
-                                         FileSystem filesystem, Configuration conf) throws IOException {
+  private void recursivelyAddFileStatus(
+      List<FileMetadata> fileMetadataList,
+      String prefix,
+      Path path,
+      Boolean recursive,
+      FileSystem filesystem,
+      Configuration conf)
+      throws IOException {
     try {
       RemoteIterator<LocatedFileStatus> iter = filesystem.listLocatedStatus(path);
       while (iter.hasNext()) {
         LocatedFileStatus fileStatus = iter.next();
         fileMetadataList.add(getFileMetadata(fileStatus, prefix, conf));
         if (fileStatus.isDirectory() && recursive) {
-          recursivelyAddFileStatus(fileMetadataList, prefix, fileStatus.getPath(), recursive, filesystem, conf);
+          recursivelyAddFileStatus(
+              fileMetadataList, prefix, fileStatus.getPath(), recursive, filesystem, conf);
         }
       }
     } catch (FileNotFoundException e) {
@@ -180,8 +186,8 @@ public class MetadataInputFormat extends InputFormat {
   }
 
   /**
-   * Returns an empty MetadataInputSplit instance. Override this method to return an InputSplit instance that works for
-   * your filesystem.
+   * Returns an empty MetadataInputSplit instance. Override this method to return an InputSplit
+   * instance that works for your filesystem.
    *
    * @return An empty MetadataInputSplit instance.
    */
@@ -190,19 +196,19 @@ public class MetadataInputFormat extends InputFormat {
   }
 
   /**
-   * Returns a FileMetadata given the FileStatus and soucePath. Override ths method to return a FileMetadata that
-   * contains additional credentials for your filesystem.
+   * Returns a FileMetadata given the FileStatus and soucePath. Override ths method to return a
+   * FileMetadata that contains additional credentials for your filesystem.
    *
    * @param fileStatus The FileStatus object that contains the raw metadata of the file
-   * @param sourcePath The source path specified by the user that was used to obtain this FileMetadata. Will be used to
-   *                   construct the relativePath field in FileMetadata.
-   * @param conf The user specified configuration that contains additional credentials required for accessing the source
-   *             filesystem.
+   * @param sourcePath The source path specified by the user that was used to obtain this
+   *     FileMetadata. Will be used to construct the relativePath field in FileMetadata.
+   * @param conf The user specified configuration that contains additional credentials required for
+   *     accessing the source filesystem.
    * @return A FileMetadata instance with its metadata fields populated
    * @throws IOException
    */
-  protected FileMetadata getFileMetadata(FileStatus fileStatus, String sourcePath, Configuration conf)
-    throws IOException {
+  protected FileMetadata getFileMetadata(
+      FileStatus fileStatus, String sourcePath, Configuration conf) throws IOException {
     return new FileMetadata(fileStatus, sourcePath);
   }
 }
