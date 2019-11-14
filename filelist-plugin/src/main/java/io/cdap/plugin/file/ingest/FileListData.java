5f7d08e6e5a6d7a19d75360756c990d7b77e429b
Pawan Goyal <pawan.goyal@amantechnologiesinc.com>
2019-11-14 12:11:33 +0530

    VCP-22 relative path and host URI added

--- filelist-plugin/src/main/java/io/cdap/plugin/file/ingest/FileListData.java
+++ filelist-plugin/src/main/java/io/cdap/plugin/file/ingest/FileListData.java
@@ -39,13 +39,17 @@ public class FileListData implements Comparable<FileListData> {
   public static final String FILE_SIZE = "fileSize";
   public static final String FULL_PATH = "fullPath";
   public static final String IS_DIR = "isDir";
+  public static final String RELATIVE_PATH = "relativePath";
+  public static final String HOST_URI = "hostURI";
 
   // The default schema that will be used to convert this object to a StructuredRecord.
   public static final Schema DEFAULT_SCHEMA =
       Schema.recordOf(
           "metadata",
           Schema.Field.of(FILE_NAME, Schema.of(Schema.Type.STRING)),
-          Schema.Field.of(FULL_PATH, Schema.of(Schema.Type.STRING)));
+          Schema.Field.of(FULL_PATH, Schema.of(Schema.Type.STRING)),
+          Schema.Field.of(RELATIVE_PATH, Schema.of(Schema.Type.STRING)),
+          Schema.Field.of(HOST_URI, Schema.of(Schema.Type.STRING)));
 
   // contains only the name of the file
   private final String fileName;
@@ -58,6 +62,32 @@ public class FileListData implements Comparable<FileListData> {
 
   // whether or not the file is a directory
   private final boolean isDir;
+
+  /*
+   * The relavite path is constructed by deleting the portion of the source
+   * path that comes before the last path separator ("/") from the full path.
+   * It is assumed here that the source path is always a prefix of the full
+   * path.
+   *
+   * For example, given full path http://example.com/foo/bar/baz/index.html
+   *              and source path /foo/bar
+   *              the relative path will be bar/baz/index.html
+   */
+  private final String relativePath;
+
+  /*
+   * URI for the Filesystem
+   * For instance, the hostURI for http://abc.def.ghi/new/index.html is http://abc.def.ghi
+   */
+  private final String hostURI;
+
+  public String getRelativePath() {
+    return relativePath;
+  }
+
+  public String getHostURI() {
+    return hostURI;
+  }
 
   /**
    * Constructs a FileListData instance given a FileStatus and source path. Override this method to
@@ -72,6 +102,25 @@ public class FileListData implements Comparable<FileListData> {
     fullPath = fileStatus.getPath().toUri().getPath();
     isDir = fileStatus.isDirectory();
     fileSize = fileStatus.getLen();
+    // check if sourcePath is a valid prefix of fullPath
+    if (fullPath.startsWith(sourcePath)) {
+      relativePath = fullPath.substring(sourcePath.lastIndexOf(Path.SEPARATOR) + 1);
+    } else {
+      throw new IOException("sourcePath should be a valid prefix of fullPath");
+    }
+
+    // construct host URI given the full path from filestatus
+    try {
+      hostURI =
+          new URI(
+                  fileStatus.getPath().toUri().getScheme(),
+                  fileStatus.getPath().toUri().getHost(),
+                  Path.SEPARATOR,
+                  null)
+              .toString();
+    } catch (URISyntaxException e) {
+      throw new IOException(e);
+    }
   }
 
   /**
@@ -85,6 +134,8 @@ public class FileListData implements Comparable<FileListData> {
     this.fullPath = record.get(FULL_PATH);
     this.fileSize = record.get(FILE_SIZE);
     this.isDir = record.get(IS_DIR);
+    this.relativePath = record.get(RELATIVE_PATH);
+    this.hostURI = record.get(HOST_URI);
   }
 
   /**
@@ -97,6 +148,8 @@ public class FileListData implements Comparable<FileListData> {
     this.fullPath = dataInput.readUTF();
     this.fileSize = dataInput.readLong();
     this.isDir = dataInput.readBoolean();
+    this.relativePath = dataInput.readUTF();
+    this.hostURI = dataInput.readUTF();
   }
 
   public String getFullPath() {
@@ -111,34 +164,24 @@ public class FileListData implements Comparable<FileListData> {
     return fileSize;
   }
 
-
-
   public boolean isDir() {
     return isDir;
   }
 
-
   /** Converts to a StructuredRecord */
   public StructuredRecord toRecord() {
-    // initialize credential schema
-    List<Schema.Field> credentialSchemaList;
-    if (getCredentialSchema() == null) {
-      credentialSchemaList = new ArrayList<>();
-    } else {
-      credentialSchemaList = getCredentialSchema().getFields();
-    }
 
     // merge default schema and credential schema to create output schema
     Schema outputSchema;
     List<Schema.Field> fieldList = new ArrayList<>(DEFAULT_SCHEMA.getFields());
-    fieldList.addAll(credentialSchemaList);
     outputSchema = Schema.recordOf("metadata", fieldList);
 
     StructuredRecord.Builder outputBuilder =
         StructuredRecord.builder(outputSchema)
             .set(FILE_NAME, fileName)
-            .set(FULL_PATH, fullPath);
-    addCredentialsToRecordBuilder(outputBuilder);
+            .set(FULL_PATH, fullPath)
+            .set(RELATIVE_PATH, relativePath)
+            .set(HOST_URI, hostURI);
 
     return outputBuilder.build();
   }
@@ -160,6 +203,8 @@ public class FileListData implements Comparable<FileListData> {
     dataOutput.writeUTF(getFullPath());
     dataOutput.writeLong(getFileSize());
     dataOutput.writeBoolean(isDir());
+    dataOutput.writeUTF(getRelativePath());
+    dataOutput.writeUTF(getHostURI());
   }
 
   /**
