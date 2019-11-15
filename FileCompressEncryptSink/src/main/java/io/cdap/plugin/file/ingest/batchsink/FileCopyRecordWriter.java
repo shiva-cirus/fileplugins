11260b1b50719e843a301099d981e540c4898c86
Shivakumar Gokaram <shiva.gokaram@amantechnologiesinc.com>
2019-11-14 23:04:25 -0500

    Change for file processing

--- FileCompressEncryptSink/src/main/java/io/cdap/plugin/file/ingest/batchsink/FileCopyRecordWriter.java
+++ FileCompressEncryptSink/src/main/java/io/cdap/plugin/file/ingest/batchsink/FileCopyRecordWriter.java
@@ -1,12 +1,12 @@
 /*
  * Copyright © 2017 Cask Data, Inc.
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *  
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -61,220 +61,215 @@ public class FileCopyRecordWriter extends RecordWriter<NullWritable, FileListDat
     private Bucket bucket = null;
 
     static Configuration conf = null;
+
     static {
         conf = new Configuration();
         conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
         conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
     }
 
-  /**
-   * Construct a RecordWriter given user configurations.
-   *
-   * @param conf The configuration that contains required information to intialize the recordWriter.
-   * @throws IOException
-   */
-  public FileCopyRecordWriter(Configuration conf) throws IOException {
+    /**
+     * Construct a RecordWriter given user configurations.
+     *
+     * @param conf The configuration that contains required information to intialize the recordWriter.
+     * @throws IOException
+     */
+    public FileCopyRecordWriter(Configuration conf) throws IOException {
 
         LOG.info("Initializating of RecordWriter");
 
-    if (conf.get(FileCopyOutputFormat.NAME_FILECOMPRESSION).equals("NONE")){
-        compression=false;
-        LOG.info("Compression is set to false");
-    } else {
-        compression=true;
-        LOG.info("Compression is set to true");
+        if (conf.get(FileCopyOutputFormat.NAME_FILECOMPRESSION).equals("NONE")) {
+            compression = false;
+            LOG.info("Compression is set to false");
+        } else {
+            compression = true;
+            LOG.info("Compression is set to true");
+        }
+
+        if (conf.get(FileCopyOutputFormat.NAME_FILEENCRYPTION).equals("NONE")) {
+            encryption = false;
+            LOG.info("Encryption is set to false");
+        } else {
+            encryption = true;
+            LOG.info("Encryption is set to true");
+
+        }
+
+        bucketname = conf.get(FileCopyOutputFormat.NAME_GCS_BUCKET, null);
+        LOG.info("Bucket Name - " + bucketname);
+
+        publicKeyPath = conf.get(FileCopyOutputFormat.NAME_PGP_PUBKEY, null);
+        LOG.info("PubKeyPath - " + publicKeyPath);
+
+        project = conf.get(FileCopyOutputFormat.NAME_GCS_PROJECTID, null);
+        LOG.info("GCS Project ID - " + project);
+
+        gcsserviceaccountjson = conf.get(FileCopyOutputFormat.NAME_GCS_SERVICEACCOUNTJSON, null);
+        LOG.info("GCS Service Account-" + gcsserviceaccountjson);
+
+        destpath = conf.get(FileCopyOutputFormat.NAME_GCS_DESTPATH, null);
+        LOG.info("Dest Path - " + destpath);
+
+        suffix = conf.get(FileCopyOutputFormat.NAME_GCS_DESTPATH_SUFFIX, null);
+        LOG.info("Suffix - " + suffix);
+
+        if (encryption) {
+            //Read the Public Key to Encrypt Data
+
+            try {
+                encKey = PGPCertUtil.readPublicKey(publicKeyPath);
+                LOG.info("Retreived PublicKey");
+            } catch (PGPException ex) {
+                LOG.error(ex.getMessage());
+                throw new IOException(ex.getMessage());
+            }
+        }
+
+        // Create GCS Storage using the credentials
+
+        storage = getGoogleStorage(gcsserviceaccountjson, project);
+        LOG.info("Created GCS Storage");
+        bucket = getBucket(storage, bucketname);
+        LOG.info("Created GCS Bucket");
+
     }
 
-    if (conf.get(FileCopyOutputFormat.NAME_FILEENCRYPTION).equals("NONE")){
-          encryption=false;
-        LOG.info("Encryption is set to false");
-    } else {
-          encryption=true;
-        LOG.info("Encryption is set to true");
+
+    private Storage getGoogleStorage(String serviceAccountJSON, String project) {
+        Credentials credentials = null;
+        try {
+            credentials = GoogleCredentials.fromStream(new FileInputStream(serviceAccountJSON));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Storage storage = StorageOptions.newBuilder()
+                .setCredentials(credentials)
+                .setProjectId(project)
+                .build()
+                .getService();
+
+        return storage;
 
     }
 
-    bucketname= conf.get( FileCopyOutputFormat.NAME_GCS_BUCKET,null );
-    LOG.info("Bucket Name - "+bucketname);
+    private Bucket getBucket(Storage storage, String bucketname) {
 
-    publicKeyPath = conf.get( FileCopyOutputFormat.NAME_PGP_PUBKEY, null);
-      LOG.info("PubKeyPath - "+publicKeyPath);
+        Bucket bucket = storage.get(bucketname);
+        if (bucket == null) {
+            System.out.println("Creating new bucket.");
+            bucket = storage.create(BucketInfo.of(bucketname));
+        }
+        return bucket;
+    }
 
-    project = conf.get( FileCopyOutputFormat.NAME_GCS_PROJECTID, null);
-    LOG.info("GCS Project ID - "+project);
 
-    gcsserviceaccountjson = conf.get( FileCopyOutputFormat.NAME_GCS_SERVICEACCOUNTJSON, null);
-    LOG.info("GCS Service Account-"+gcsserviceaccountjson);
+    /**
+     * This method connects to the source filesystem and copies the file specified by the FileMetadata input to the
+     * destination filesystem.
+     *
+     * @param key          Unused key.
+     * @param fileListData Contains metadata for the file we wish to copy.
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Override
+    public void write(NullWritable key, FileListData fileListData) throws IOException, InterruptedException {
 
-    destpath = conf.get( FileCopyOutputFormat.NAME_GCS_DESTPATH , null);
-    LOG.info("Dest Path - "+destpath);
+        if (fileListData.getRelativePath().isEmpty()) {
+            return;
+        }
 
-    suffix = conf.get( FileCopyOutputFormat.NAME_GCS_DESTPATH_SUFFIX , null);
-      LOG.info("Suffix - "+suffix);
+        // construct file paths for source and destination
+        //Path srcPath = new Path(fileMetadata.getFullPath());
 
-    if (encryption) {
-        //Read the Public Key to Encrypt Data
+        String outFileName = destpath + fileListData.getRelativePath();
+        String contentType = "application/octet-stream";
+        if (compression) {
+            outFileName += ".zip";
+            contentType = "application/zip";
+        }
+
+
+        if (encryption) {
+            outFileName += ".pgp";
+            contentType = "application/pgp-encrypted";
+        }
+
+        FileMetaData fileMetaData = null;
+        String fileName = fileListData.getFullPath();
+        if (fileName != null) {
+            try {
+                fileMetaData = getFileMetaData(fileName, fileListData.getHostURI());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        LOG.info("Output File Name " + outFileName);
+
+        BlobId blobId = BlobId.of(bucket.getName(), outFileName);
+
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType(contentType).build();
+
+
+        InputStream inputStream = null;
 
         try {
-            encKey = PGPCertUtil.readPublicKey(publicKeyPath);
-            LOG.info("Retreived PublicKey");
-        }catch (PGPException ex) {
-            LOG.error(ex.getMessage());
-            throw new IOException(ex.getMessage());
+            inputStream = FileCompressEncrypt.gcsWriter(fileMetaData, compression, encryption, encKey);
+            byte[] buffer = new byte[1 << 16];
+            try (WriteChannel writer =
+                         storage.writer(blobInfo)) {
+                int limit;
+                while ((limit = inputStream.read(buffer)) >= 0) {
+                    System.out.println("upload file " + limit);
+                    writer.write(ByteBuffer.wrap(buffer, 0, limit));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }
-
-    // Create GCS Storage using the credentials
-
-      storage = getGoogleStorage(gcsserviceaccountjson,project);
-      LOG.info("Created GCS Storage");
-      bucket = getBucket(storage,bucketname);
-      LOG.info("Created GCS Bucket");
-
-  }
-
-
-
-  private Storage getGoogleStorage(String serviceAccountJSON, String project) {
-    Credentials credentials = null;
-    try {
-      credentials = GoogleCredentials.fromStream(new FileInputStream(serviceAccountJSON));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-    Storage storage = StorageOptions.newBuilder()
-            .setCredentials(credentials)
-            .setProjectId(project)
-            .build()
-            .getService();
-
-    return storage;
-
-  }
-
-  private Bucket getBucket(Storage storage,String bucketname) {
-
-    Bucket bucket = storage.get(bucketname);
-    if (bucket == null) {
-      System.out.println("Creating new bucket.");
-      bucket = storage.create(BucketInfo.of(bucketname));
-    }
-    return bucket;
-  }
-
-
-
-
-
-
-  /**
-   * This method connects to the source filesystem and copies the file specified by the FileMetadata input to the
-   * destination filesystem.
-   *
-   * @param key Unused key.
-   * @param fileListData Contains metadata for the file we wish to copy.
-   * @throws IOException
-   * @throws InterruptedException
-   */
-  @Override
-  public void write(NullWritable key, FileListData fileListData) throws IOException, InterruptedException {
-
-    if (fileListData.getRelativePath().isEmpty()) {
-      return;
-    }
-
-    // construct file paths for source and destination
-    //Path srcPath = new Path(fileMetadata.getFullPath());
-
-    String outFileName = destpath + fileListData.getRelativePath();
-      String contentType = "application/octet-stream";
-      if (encryption) {
-          outFileName += ".pgp";
-          contentType = "application/pgp-encrypted";
-      }
-
-      FileMetaData fileMetaData = null;
-      String fileName = fileListData.getFullPath();
-      if (fileName != null) {
-          try {
-              fileMetaData = getFileMetaData(fileName, "");
-          } catch (IOException e) {
-              e.printStackTrace();
-          }
-
-      }
-
-
-
-
-    LOG.info("Output File Name " + outFileName);
-
-      BlobId blobId = BlobId.of(bucket.getName(), outFileName);
-
-      BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType(contentType).build();
-
-
-      InputStream inputStream = null;
-
-      try {
-          inputStream = FileCompressEncrypt.gcsWriter(fileMetaData,compression ,encryption , encKey);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-
-      byte[] buffer = new byte[1 << 16];
-      try {
-        try (WriteChannel writer =
-                     storage.writer(blobInfo)) {
-          int limit;
-          while ((limit = inputStream.read(buffer)) >= 0) {
-            System.out.println("upload file " + limit);
-            writer.write(ByteBuffer.wrap(buffer, 0, limit));
-          }
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      try {
-        inputStream.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
 
 
     }
 
 
     private static FileMetaData getFileMetaData(String filePath, String uri) throws IOException {
-        return new FileMetaData(filePath, conf);
+        return new FileMetaData(uri+'/'+filePath, conf);
     }
 
-  @Override
-  public void close(TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
-    // attempts to close the other even if one fails
-   // try {
-   //   destFileSystem.close();
-   // } finally {
-    //  safelyCloseSourceFilesystems(sourceFilesystemMap.values().iterator());
-   // }
-  }
-
-  /**
-   * this method attempts to close every Filesystem object in the list, logs a warning
-   * for each object that fails to close
-   *
-   * @param fs The iterator over all the Filesystems we wish to close.
-   */
-  private void safelyCloseSourceFilesystems(Iterator<FileSystem> fs) {
-    while (fs.hasNext()) {
-      try {
-        fs.next().close();
-      } catch (IOException e) {
-        LOG.warn(e.getMessage());
-      }
+    @Override
+    public void close(TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
+        // attempts to close the other even if one fails
+        // try {
+        //   destFileSystem.close();
+        // } finally {
+        //  safelyCloseSourceFilesystems(sourceFilesystemMap.values().iterator());
+        // }
     }
-  }
+
+    /**
+     * this method attempts to close every Filesystem object in the list, logs a warning
+     * for each object that fails to close
+     *
+     * @param fs The iterator over all the Filesystems we wish to close.
+     */
+    private void safelyCloseSourceFilesystems(Iterator<FileSystem> fs) {
+        while (fs.hasNext()) {
+            try {
+                fs.next().close();
+            } catch (IOException e) {
+                LOG.warn(e.getMessage());
+            }
+        }
+    }
 
 
 }
