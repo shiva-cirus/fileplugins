a1acf1bb71cf1d18e270d9d9d8637ee53b144917
Pawan Goyal <pawan.goyal@amantechnologiesinc.com>
2019-12-05 10:56:12 +0530

    refactoring to remove extra field

--- FileDeCompressDeCryptBatchSource/src/main/java/io/cdap/plugin/file/ingest/FileRecordReader.java
+++ FileDeCompressDeCryptBatchSource/src/main/java/io/cdap/plugin/file/ingest/FileRecordReader.java
@@ -72,40 +72,41 @@ public class FileRecordReader extends RecordReader<Long, CSVRecord> {
     Configuration configuration = taskAttemptContext.getConfiguration();
     String filePath = this.split.getFileMetaDataList().get(0).getFullPath();
     String privateKeyFilePath = configuration.get("privateKeyFilePath");
-    char[] privateKeyPassword = configuration.get("password").toCharArray(); // "passphrase";
-    Boolean decrypt = configuration.getBoolean("decrypt", false);
-    Boolean decompress = configuration.getBoolean("decompress", false);
+    char[] privateKeyPassword = configuration.get("password").toCharArray();
+    String decryptionAlgorithm = configuration.get("decryptionAlgorithm");
+    String decompressionFormat = configuration.get("decompressionFormat");
     try {
-      if (decrypt) {
-        if (decompress) {
+      if (decryptionAlgorithm.equalsIgnoreCase("PGP")) {
+        if (decompressionFormat.equalsIgnoreCase("ZIP")) {
           inputFileStream =
               FileUtil.decryptAndDecompress(filePath, privateKeyFilePath, privateKeyPassword);
-        } else {
+        } else if (decompressionFormat.equalsIgnoreCase("NONE")) {
           inputFileStream = FileUtil.decrypt(filePath, privateKeyFilePath, privateKeyPassword);
         }
-      } else {
-        if (decompress) {
+      } else if (decryptionAlgorithm.equalsIgnoreCase("NONE")) {
+        if (decompressionFormat.equalsIgnoreCase("ZIP")) {
           ZipFile zf = new ZipFile(filePath);
           Enumeration entries = zf.entries();
           ZipEntry ze = (ZipEntry) entries.nextElement();
           inputFileStream = zf.getInputStream(ze);
-        } else {
-          inputFileStream = new FileInputStream(filePath);
+        } else if (decompressionFormat.equalsIgnoreCase("NONE")) {
+          {
+            inputFileStream = new FileInputStream(filePath);
+          }
         }
+
+        isReader = new InputStreamReader(inputFileStream);
+        // Creating a BufferedReader object
+        reader = new BufferedReader(isReader);
+
+        CSVParser csvParser =
+            new CSVParser(
+                reader,
+                CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());
+
+        rows = csvParser.iterator();
+        rowIdx = 0;
       }
-
-      isReader = new InputStreamReader(inputFileStream);
-      // Creating a BufferedReader object
-      reader = new BufferedReader(isReader);
-
-      CSVParser csvParser =
-          new CSVParser(
-              reader,
-              CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());
-
-      rows = csvParser.iterator();
-      rowIdx = 0;
-
     } catch (IOException e) {
       e.printStackTrace();
     } catch (NoSuchProviderException e) {
